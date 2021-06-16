78c6247b21afff9caae45cb692c2ce742e4b70e5
hauvan <hau.vd1606@gmail.com>
2021-06-16 16:02:29 +0700

    update code

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const Button = ({ children }) => {
-  console.log("process-------:", process.env);
   return (
     <>
       <button className={styles.root}>{children}</button>
